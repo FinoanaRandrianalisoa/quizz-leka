9afd7b82a8592dedcf371a452377fc745b8ea529
Finoana <randrianalisoafinoana@gmail.com>
2026-09-11 14:57:34 +0300

    Revert temporary Railway workarounds now that Procfile handles migrations

--- apps/quiz_global/models.py
+++ apps/quiz_global/models.py
@@ -41,7 +41,7 @@ class QuizGlobalGame(TimeStampedModel):
     phase_deadline = models.DateTimeField(null=True, blank=True)
     started_at = models.DateTimeField(null=True, blank=True)
     finished_at = models.DateTimeField(null=True, blank=True)
-    # expired_at = models.DateTimeField(null=True, blank=True)  # Temporarily commented due to Railway migration issue
+    expired_at = models.DateTimeField(null=True, blank=True)
     abandoned_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
